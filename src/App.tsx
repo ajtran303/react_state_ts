2710254f2a6ea8db6d746a471afca16a40d3d515
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2023-06-24 22:19:06 -0400

    Make Counter its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Counter from "./Counter"
+
 function App() {
 
   const containerStyle = {
@@ -13,77 +15,61 @@ function App() {
       </header>
 
       <main>
-        <article>
-          <h2>Article Title 1</h2>
+        <Counter />
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt 
-            aliquam ab accusantium vero cupiditate perferendis, provident quibusdam 
-            sequi porro at quidem, excepturi voluptate impedit numquam eius molestias ea earum?
-          </p>
+        <section className="article-carousel">
+          <h2>Article Carousel Example</h2>
 
-          <blockquote cite="https://example.com/">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Odio ut enim 
-            blandit volutpat maecenas volutpat blandit aliquam etiam.
-          </blockquote>
-        </article>
+          <button>Previous Article</button>
+          <button>Next Article</button>
 
-        <article>
-          <h2>Article Title 2</h2>
+          <article>
+            <h3>Article Title 1</h3>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt 
-            aliquam ab accusantium vero cupiditate perferendis, provident quibusdam 
-            sequi porro at quidem, excepturi voluptate impedit numquam eius molestias ea earum?
-          </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt 
+              aliquam ab accusantium vero cupiditate perferendis, provident quibusdam 
+              sequi porro at quidem, excepturi voluptate impedit numquam eius molestias ea earum?
+            </p>
 
-          <blockquote cite="https://example.com/">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Odio ut enim 
-            blandit volutpat maecenas volutpat blandit aliquam etiam.
-          </blockquote>
-        </article>
-
-
-        <article>
-          <h2>Article Title 3</h2>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt 
-            aliquam ab accusantium vero cupiditate perferendis, provident quibusdam 
-            sequi porro at quidem, excepturi voluptate impedit numquam eius molestias ea earum?
-          </p>
-
-          <blockquote cite="https://example.com/">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Odio ut enim 
-            blandit volutpat maecenas volutpat blandit aliquam etiam.
-          </blockquote>
-        </article>
-
-        <section className="bulletpoints">
-          <h3>Section Title</h3>
-
-          <ul>
-            <li>
+            <blockquote>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-              tempor incididunt ut labore et dolore magna aliqua. Elementum integer enim 
-              neque volutpat ac tincidunt vitae semper. <a href="https://example.com/" target="blank">Click me</a>
-            </li>
+              tempor incididunt ut labore et dolore magna aliqua. Odio ut enim 
+              blandit volutpat maecenas volutpat blandit aliquam etiam.
+            </blockquote>
+          </article>
 
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-              tempor incididunt ut labore et dolore magna aliqua. Elementum integer enim 
-              neque volutpat ac tincidunt vitae semper. <a href="https://example.com/" target="blank">Click me</a>
-            </li>
+          <article>
+            <h3>Article Title 2</h3>
+            
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt 
+              aliquam ab accusantium vero cupiditate perferendis, provident quibusdam 
+              sequi porro at quidem, excepturi voluptate impedit numquam eius molestias ea earum?
+            </p>
 
-            <li>
+            <blockquote>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-              tempor incididunt ut labore et dolore magna aliqua. Elementum integer enim 
-              neque volutpat ac tincidunt vitae semper. <a href="https://example.com/" target="blank">Click me</a>
-            </li>
-          </ul>
+              tempor incididunt ut labore et dolore magna aliqua. Odio ut enim 
+              blandit volutpat maecenas volutpat blandit aliquam etiam.
+            </blockquote>
+          </article>
+
+          <article>
+            <h3>Article Title 3</h3>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt 
+              aliquam ab accusantium vero cupiditate perferendis, provident quibusdam 
+              sequi porro at quidem, excepturi voluptate impedit numquam eius molestias ea earum?
+            </p>
+
+            <blockquote>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+              tempor incididunt ut labore et dolore magna aliqua. Odio ut enim 
+              blandit volutpat maecenas volutpat blandit aliquam etiam.
+            </blockquote>
+          </article>
         </section>
       </main>
 
---
+++ src/Counter.tsx
@@ -0,0 +1,15 @@
+function Counter() {
+  return (
+    <section className="counter">
+      <h2>Counter Example</h2>
+
+      <p>
+        The current value is: ???
+      </p>
+
+      <button>Increment value</button>
+    </section>
+  )
+}
+
+export default Counter
