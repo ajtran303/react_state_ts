a375292eaed33eb1c02bd2e87a5fece3fd33d59c
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2023-06-26 20:28:11 -0400

    Simplify carousel component

--- src/ArticleCarousel.tsx
+++ src/ArticleCarousel.tsx
@@ -1,3 +1,45 @@
+const article1 = (
+  <article>
+    <h3>Proin Risus</h3>
+
+    <p>
+      Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+    </p>
+
+    <blockquote>
+      Sed ante. Vivamus tortor. Duis mattis egestas metus.
+    </blockquote>
+  </article>
+)
+
+const article2 = (
+  <article>
+    <h3>Donec ut Dolor</h3>
+
+    <p>
+      Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+    </p>
+
+    <blockquote>
+      Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
+    </blockquote>
+  </article>
+)
+
+const article3 = (
+  <article>
+    <h3>Pellentesque Ultrices Mattis Odio</h3>
+
+    <p>
+      Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+    </p>
+
+    <blockquote>
+      Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
+    </blockquote>
+  </article>
+)
+
 function ArticleCarousel() {
   return (
     <section className="article-carousel">
@@ -6,41 +48,9 @@ function ArticleCarousel() {
       <button>Previous Article</button>
       <button>Next Article</button>
 
-      <article>
-        <h3>Proin Risus</h3>
-
-        <p>
-          Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
-        </p>
-
-        <blockquote>
-          Sed ante. Vivamus tortor. Duis mattis egestas metus.
-        </blockquote>
-      </article>
-
-      <article>
-        <h3>Donec ut Dolor</h3>
-        
-        <p>
-          Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
-        </p>
-
-        <blockquote>
-          Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
-        </blockquote>
-      </article>
-
-      <article>
-        <h3>Pellentesque Ultrices Mattis Odio</h3>
-
-        <p>
-          Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
-        </p>
-
-        <blockquote>
-          Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
-        </blockquote>
-      </article>
+      {article1}
+      {article2}
+      {article3}
     </section>
   )
 }
